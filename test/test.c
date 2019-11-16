7fa2e516d6679b5172febc367891ecaa91efeea5
Ed Younis <edyounis123@gmail.com>
2019-11-16 15:50:18 -0800

    Testing Started

--- test/test.c
+++ test/test.c
@@ -1,8 +1,17 @@
 // source: https://stackoverflow.com/questions/13513905/how-to-set-up-googletest-as-a-shared-library-on-linux
 
 #include <gtest/gtest.h>
-TEST(MathTest, TwoPlusTwoEqualsFour) {
-	EXPECT_EQ(2 + 2, 4);
+#include <vectors.h>
+
+TEST(VectorRegisterTest, DefaultConstructor)
+{
+	xavier::VectorRegister x;
+	for ( int i = 0; i < 16; ++i )
+		ASSERT_EQ( x.internal.elems[i], 0 );
+	#ifdef __AVX2__
+		for ( int i = 16; i < 32; ++i )
+			ASSERT_EQ( x.internal.elems[i], 0 );
+	#endif
 }
 
 int main(int argc, char **argv) {
