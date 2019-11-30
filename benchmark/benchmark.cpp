43c98ef7f22f6d8bb75493087855002d7a0759fc
Giulia Guidi <gl.guidi93@gmail.com>
2019-11-29 17:16:16 -0800

    iter in benchmark

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -174,16 +174,18 @@ int main(int argc, char const *argv[])
 	/* Seed starting position on seq1, seed starting position on seq2, k-mer lenght */
 	xavier::Seed seed(0, 0, 17);
 
-	// 1st prototype without seed and x-drop termination
-	std::chrono::duration<double> diff1;
-	auto start1 = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 13; i++)
+	{
+		// 1st prototype without seed and x-drop termination
+		std::chrono::duration<double> diff1;
+		auto start1 = std::chrono::high_resolution_clock::now();
 
-	xavier::AlignmentResult result = xavier::seed_and_extend_right( targetSeg, querySeg, penalties, xdrop, seed );
+		xavier::AlignmentResult result = xavier::seed_and_extend_right( targetSeg, querySeg, penalties, xdrop, seed );
 
-	auto end1 = std::chrono::high_resolution_clock::now();
-	diff1 = end1-start1;
-	std::cout << "X	" << result.bestScore << "	" << diff1.count() << "	" <<  (double)len1 / diff1.count() << std::endl;
-
+		auto end1 = std::chrono::high_resolution_clock::now();
+		diff1 = end1-start1;
+		std::cout << "X	" << result.bestScore << "	" << diff1.count() << "	" <<  (double)len1 / diff1.count() << std::endl;
+	}
 	//======================================================================================
 	// KSW2 GLOBAL AND EXTENSION (vectorized SSE4.1 and x-drop, not banded)
 	//======================================================================================
@@ -215,17 +217,20 @@ int main(int argc, char const *argv[])
 		qs[i] = c[(uint8_t)querySeg[i]];
 	}
 
-	std::chrono::duration<double> diff2;
-	auto start2 = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 13; i++)
+	{
+		std::chrono::duration<double> diff2;
+		auto start2 = std::chrono::high_resolution_clock::now();
 
-	ksw_extz2_sse(0, ql, qs, tl, ts, 5, mat, 0, -GAP, bw, xdrop, 0, KSW_EZ_SCORE_ONLY, &ez);
+		ksw_extz2_sse(0, ql, qs, tl, ts, 5, mat, 0, -GAP, bw, xdrop, 0, KSW_EZ_SCORE_ONLY, &ez);
 
-	auto end2 = std::chrono::high_resolution_clock::now();
-	diff2 = end2-start2;
+		auto end2 = std::chrono::high_resolution_clock::now();
+		diff2 = end2-start2;
+
+		std::cout << "K	" << ez.score << "	" << diff2.count() << "	" << (double)len1 / diff2.count() << std::endl;
+	}
 
 	free(ts); free(qs);
-
-	std::cout << "K	" << ez.score << "	" << diff2.count() << "	" << (double)len1 / diff2.count() << std::endl;
 #endif
 
 	//======================================================================================
@@ -242,50 +247,53 @@ int main(int argc, char const *argv[])
 		filter_thresh : 0,
 	));
 
-	std::chrono::duration<double> diff3;
-	auto start3 = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 13; i++)
+	{
+		std::chrono::duration<double> diff3;
+		auto start3 = std::chrono::high_resolution_clock::now();
 
-	char const t[64] = {0};	// tail array
-	gaba_section_t asec = gaba_build_section(0, (uint8_t const *)targetSeg.c_str(), (uint32_t)targetSeg.size());
-	gaba_section_t bsec = gaba_build_section(2, (uint8_t const *)querySeg.c_str(),  (uint32_t)querySeg.size());
-	gaba_section_t tail = gaba_build_section(4, t, 64);
+		char const t[64] = {0};	// tail array
+		gaba_section_t asec = gaba_build_section(0, (uint8_t const *)targetSeg.c_str(), (uint32_t)targetSeg.size());
+		gaba_section_t bsec = gaba_build_section(2, (uint8_t const *)querySeg.c_str(),  (uint32_t)querySeg.size());
+		gaba_section_t tail = gaba_build_section(4, t, 64);
 
-	// create thread-local object
-	gaba_dp_t *dp = gaba_dp_init(ctx);	// dp[0] holds a 64-cell-wide context
+		// create thread-local object
+		gaba_dp_t *dp = gaba_dp_init(ctx);	// dp[0] holds a 64-cell-wide context
 
-	// init section pointers
-	gaba_section_t const *ap = &asec, *bp = &bsec;
-	gaba_fill_t const *f = gaba_dp_fill_root(dp, // dp -> &dp[_dp_ctx_index(band_width)] makes the band width selectable
-		ap, 0,					// a-side (reference side) sequence and start position
-		bp, 0,					// b-side (query)
-		UINT32_MAX				// max extension length
-	);
+		// init section pointers
+		gaba_section_t const *ap = &asec, *bp = &bsec;
+		gaba_fill_t const *f = gaba_dp_fill_root(dp, // dp -> &dp[_dp_ctx_index(band_width)] makes the band width selectable
+			ap, 0,					// a-side (reference side) sequence and start position
+			bp, 0,					// b-side (query)
+			UINT32_MAX				// max extension length
+		);
 
-	// until x-drop condition is detected
-	gaba_fill_t const *m = f;
+		// until x-drop condition is detected
+		gaba_fill_t const *m = f;
 
-	while((f->status & GABA_TERM) == 0) {
-		// substitute the pointer by the tail section's if it reached the end
-		if(f->status & GABA_UPDATE_A) { ap = &tail; }
-		if(f->status & GABA_UPDATE_B) { bp = &tail; }
+		while((f->status & GABA_TERM) == 0) {
+			// substitute the pointer by the tail section's if it reached the end
+			if(f->status & GABA_UPDATE_A) { ap = &tail; }
+			if(f->status & GABA_UPDATE_B) { bp = &tail; }
 
-		f = gaba_dp_fill(dp, f, ap, bp, UINT32_MAX);	// extend the banded matrix
-		m = f->max > m->max ? f : m;					// swap if maximum score was updated
+			f = gaba_dp_fill(dp, f, ap, bp, UINT32_MAX);	// extend the banded matrix
+			m = f->max > m->max ? f : m;					// swap if maximum score was updated
+		}
+
+		// alignment path
+		gaba_alignment_t *r = gaba_dp_trace(dp,
+			m,		// section with the max
+			NULL	// custom allocator: see struct gaba_alloc_s in gaba.h
+		);
+
+		auto end3 = std::chrono::high_resolution_clock::now();
+		diff3 = end3-start3;
+
+		std::cout << "G	" << r->score << "	" << diff3.count() << "	" << (double)len1 / diff3.count() << std::endl;
+
+		// clean up
+		gaba_dp_res_free(dp, r); gaba_dp_clean(dp);
 	}
-
-	// alignment path
-	gaba_alignment_t *r = gaba_dp_trace(dp,
-		m,		// section with the max
-		NULL	// custom allocator: see struct gaba_alloc_s in gaba.h
-	);
-
-	auto end3 = std::chrono::high_resolution_clock::now();
-	diff3 = end3-start3;
-
-	std::cout << "G	" << r->score << "	" << diff3.count() << "	" << (double)len1 / diff3.count() << std::endl;
-
-	// clean up
-	gaba_dp_res_free(dp, r); gaba_dp_clean(dp);
 	gaba_clean(ctx);
 
 #endif
@@ -304,16 +312,18 @@ int main(int argc, char const *argv[])
 	StripedSmithWaterman::Filter filter;
 	filter.report_begin_position = false;
 	filter.report_cigar = false;
+	for (int i = 0; i < 13; i++)
+	{
 
-	std::chrono::duration<double> diff5;
-	auto start5 = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> diff5;
+		auto start5 = std::chrono::high_resolution_clock::now();
 
-	aligner.Align(querySeg.c_str(), targetSeg.c_str(), targetSeg.size(), filter, &alignment, maskLen);
+		aligner.Align(querySeg.c_str(), targetSeg.c_str(), targetSeg.size(), filter, &alignment, maskLen);
 
-	auto end5 = std::chrono::high_resolution_clock::now();
-	diff5 = end5-start5;
-
-	std::cout << "S	" << alignment.sw_score << "	" << diff5.count() << "	" << (double)len1 / diff5.count() << std::endl;
+		auto end5 = std::chrono::high_resolution_clock::now();
+		diff5 = end5-start5;
+		std::cout << "S	" << alignment.sw_score << "	" << diff5.count() << "	" << (double)len1 / diff5.count() << std::endl;
+	}
 #endif
 
 	//======================================================================================
@@ -345,6 +355,9 @@ int main(int argc, char const *argv[])
 	const char alphabet[4] = {'A','T','C', 'G'};
 	const parasail_matrix_t *matrix = parasail_matrix_create(alphabet, MAT, MIS);
 
+	for (int i = 0; i < 13; i++)
+	{
+
 	std::chrono::duration<double> diff6;
 	auto start6 = std::chrono::high_resolution_clock::now();
 
@@ -355,6 +368,7 @@ int main(int argc, char const *argv[])
 
 	std::cout << "P1	" << parasail_result_get_score(result1) << "	" << diff6.count() << "	" << (double)len1 / diff6.count() << std::endl;
 	parasail_result_free(result1);
+	}
 #endif
 
 	//======================================================================================
@@ -363,6 +377,8 @@ int main(int argc, char const *argv[])
 
 #ifdef PARASAIL2
 	parasail_result_t *result2 = NULL;
+	for (int i = 0; i < 13; i++)
+	{
 
 	std::chrono::duration<double> diff8;
 	auto start8 = std::chrono::high_resolution_clock::now();
@@ -375,6 +391,7 @@ int main(int argc, char const *argv[])
 	std::cout << "P2	" << parasail_result_get_score(result2) << "	" << diff8.count() << "	" << (double)len1 / diff8.count() << std::endl;
 	std::cout << std::endl;
 	parasail_result_free(result2);
+	}
 #endif
 	return 0;
 }
