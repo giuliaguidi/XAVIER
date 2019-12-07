06325ab5743ad4c5d14cf85674eb147b5d4a0f5a
Giulia Guidi <gl.guidi93@gmail.com>
2019-12-06 16:50:14 -0800

    xdrop not trivia proceding bottom-ip and left-right

--- benchmark/align.py
+++ benchmark/align.py
@@ -6,7 +6,7 @@ from semiglobalAlign import*
 def main():
     
     #get command args
-    # seq1Name,seq2Name,xdrop,matchScore,mismatchPenalty,gapPenalty,outFile
+    #seq1Name,seq2Name,xdrop,matchScore,mismatchPenalty,gapPenalty,outFile
     commandArgs = readCommand(sys.argv)
 
     #load sequence
@@ -22,7 +22,7 @@ def main():
     output = open(commandArgs[6],'w+')
     for i in range(0, len(seq1)):
         result = []
-        result = semiglobalAlign(seq1[i], seq2[i], matchScore, mismatchPenalty, gapPenalty)
+        result = semiglobalAlign(seq1[i], seq2[i], matchScore, mismatchPenalty, gapPenalty, xdrop)
 
         #output
         for item in result:
@@ -43,11 +43,11 @@ def main():
             print("\n"+seq1[i]+": "+str(item[4])+" "+item[0]+" "+str(item[4]+len(item[0][0:60].replace("_",""))))
             print(seq2[i]+": "+str(item[5])+" "+item[1]+" "+str(item[5]+len(item[0][0:60].replace("_","")))+"\n")
             print("Score: "+str(item[2]))
-            print("Identities: "+str(item[3])+"/"+str(len(item[0]))+" ("+str(item[3]*100//len(item[0]))+"%)\n")
+            print("Identity: "+str(item[3])+"/"+str(len(item[0]))+" ("+str(item[3]*100//len(item[0]))+"%)\n")
             output.write(seq1[i]+": "+str(item[4])+" "+item[0]+" "+str(item[4]+len(item[0][0:60].replace("_","")))+"\n")
             output.write(seq2[i]+": "+str(item[5])+" "+item[1]+" "+str(item[5]+len(item[0][0:60].replace("_","")))+"\n")
             output.write("\nScore: "+str(item[2])+"\n")
-            output.write("Identities: "+str(item[3])+"/"+str(len(item[0]))+" ("+str(item[3]*100//len(item[0]))+"%)\n\n")
+            output.write("Identity: "+str(item[3])+"/"+str(len(item[0]))+" ("+str(item[3]*100//len(item[0]))+"%)\n\n")
     output.close()
 
 main()
