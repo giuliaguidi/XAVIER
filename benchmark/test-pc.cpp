6f239e209f56debf8bdf3c12cb98e76503487e61
Giulia Guidi <gl.guidi93@gmail.com>
2020-02-07 23:14:24 -0800

    pc test in place; need to fix conflict with seqan

--- benchmark/test-pc.cpp
+++ benchmark/test-pc.cpp
@@ -1,5 +1,5 @@
 //==============================================================================
-// Title:   Xavier: High-Performance X-Drop Adaptive Banded Pairwise Alignment
+// Title:   Xavier: High-Performance xy[i].first-Drop Adaptive Banded Pairwise Alignment
 // Author:  G. Guidi, E. Younis
 // Date:    7 February 2020
 // Test:    
@@ -30,11 +30,33 @@
 #include <iterator>
 #include <x86intrin.h>
 #include <random>
+#include <bits/stdc++.h> 
 
 #include "utils.h"
 #include "software.h"
 
-// TODO: design test pearson correlation
+// pearson correlation coefficient
+float pc(std::vector<std::pair<int, int>> xy) 
+{ 
+	int n = xy.length();
+
+    int sumx = 0, sumy = 0, sumxy = 0; 
+    int sum2x = 0, sum2y = 0; 
+  
+    for (int i = 0; i < n; i++) 
+    { 
+        sumx 	= sumx 	+ xy[i].first; 
+        sumy 	= sumy 	+ xy[i].second; 
+        sumxy 	= sumxy + xy[i].first * xy[i].second; 
+  
+        sum2x 	= sum2x + xy[i].first 	* xy[i].first; 
+        sum2y 	= sum2y + xy[i].second 	* xy[i].second; 
+    } 
+  
+    float pcorr = (float)(n * sumxy - sumx * sumy) / std::sqrt((n * sum2x - sumx * sumx) * (n * sum2y - sumy * sumy)); 
+
+    return pcorr; 
+}
 
 int main(int argc, char const *argv[])
 {
@@ -99,6 +121,21 @@ int main(int argc, char const *argv[])
 		copy(vgpc[t].begin(), vgpc[t].end(), gpc.begin() + gpcsofar);
 		gpcsofar += vgpc[t].size();
 	}
+
+	for(int i = 0; i < iter; i++)
+		std::cout << xpc[i].first << "\t" << xpc[i].second << std::endl;
+	float pcx = pc(xpc);
+	std::cout << pcx << "\n" << std::endl;
+
+	for(int i = 0; i < iter; i++)
+		std::cout << kpc[i].first << "\t" << kpc[i].second << std::endl;
+	float pck = pc(kpc);
+	std::cout << pck << "\n" << std::endl;
+
+	for(int i = 0; i < iter; i++)
+		std::cout << gpc[i].first << "\t" << gpc[i].second << std::endl;
+	float pcg = pc(gpc);
+	std::cout << pcg << "\n" << std::endl;
 
 	return 0;
 }
